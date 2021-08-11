60f9117d76b0246464adb2a2bbd4104128d10b29
Arnaut <lou.arnaut@gmail.com>
2021-08-10 20:02:32 -0700

    added more queries, and signup, login, add book mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            password
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query user($email: String!) {
+        user(email: $email) {
+            _id
+            username
+            email
+            savedBooks{
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
+
+export const QUERY_BOOK = gql`
+    query book($id: ID!) {
+        book(_id: $id) {
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+`;
+
+export const QUERY_BOOKS = gql`
+    query books($email: String){
+        books(email: $email) {
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+`;
