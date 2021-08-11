5c8f3bdfa424e8c68a5e6d9a200b106ae582b408
Arnaut <lou.arnaut@gmail.com>
2021-08-10 18:48:36 -0700

    created resolvers for user and book, as well as mutation for addUser

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,35 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const { Thought } = require('../../../deep-thoughts/server/models');
+
+const resolvers = {
+    Query: {
+        /* Get logged user info */
+        me: async ( parent, args, context ) => {
+            if (context.user) {
+                const userData = await User.findOne(
+                    { _id: context.user_id }
+                )
+                .select('--v -password')
+                .populate('savedBooks');
+
+                return userData;
+            }
+            throw new AuthenticationError('You\'re not logged in');
+        },
+
+        /* Get book by Id*/
+        book: async ( parent, { _id }) => {
+            return Thought.findOne({ _id })
+        }
+    },
+    Mutation: {
+        /* Sign up (add) user */
+        addUser: async ( parent, args ) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user};
+        }
+    }
+}
