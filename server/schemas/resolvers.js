0bd71da3fca52f498fde9422d0e5a3f80746fdae
Arnaut <lou.arnaut@gmail.com>
2021-08-10 18:51:27 -0700

    added login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -30,6 +30,25 @@ const resolvers = {
             const user = await User.create(args);
             const token = signToken(user);
             return { token, user};
+        },
+
+        /* Login user */
+        login: async ( parent, { email, password } ) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
         }
     }
 }
