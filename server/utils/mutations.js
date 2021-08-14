32817947fba47b3cb0d57a7c101ee97f92a68c28
Arnaut <lou.arnaut@gmail.com>
2021-08-14 01:36:13 -0700

    updated utils/mutations

--- server/utils/mutations.js
+++ server/utils/mutations.js
@@ -24,59 +24,29 @@ export const ADD_USER = gql`
   }
 `;
 
-export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-      }
+export const ADD_BOOK = gql`
+  mutation addBook(bookId: String!, title: String!, authors:[String]!, description: String!, image: String!, link: String!) {
+    addBook(bookId: $bookId, title: $title, authors: $authors, description: $description, image: $image, link: $image) {
+        bookId
+        title
+        authors
+        description
+        image
+        link
     }
   }
 `;
 
-export const ADD_REACTION = gql`
-  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
-    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+export const REMOVE_BOOK = gql`
+  mutation removeBook($id: ID!) {
+    removeBook(_id: $id) {
       _id
-      reactionCount
-      reactions {
-        _id
-        reactionBody
-        createdAt
-        username
-      }
-    }
-  }
-`;
-
-export const ADD_FRIEND = gql`
-  mutation addFriend($id: ID!) {
-    addFriend(friendId: $id) {
-      _id
-      username
-      friendCount
-      friends {
-        _id
-        username
-      }
-    }
-  }
-`;
-
-export const REMOVE_FRIEND = gql`
-  mutation removeFriend($id: ID!) {
-    removeFriend(id: $id) {
-      _id
-      username
-      friends {
-        _id
-        username
-      }
+      bookId
+      title
+      authors
+      description
+      image
+      link
     }
   }
 `;
